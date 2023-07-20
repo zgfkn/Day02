4c5919697edec117c794bba797e4cb87759d9790
zax <2114531580@qq.com>
2023-07-20 10:57:18 +0800

    第二天

---
+++ src/eg01/OperatorDemo.java
@@ -0,0 +1,14 @@
+package eg01;
+
+/**
+ * 运算符介绍
+ */
+public class OperatorDemo {
+    public static void main(String[] args) {
+        //运算符的优先级
+        int a = 2 + 2 / 2 + 3 * 5 + 4;
+        System.out.println(a);
+        //优先级相同,从左到右依次计算,有括号先算括号里面的
+
+    }
+}
