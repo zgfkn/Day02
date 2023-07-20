31673b2e128a0e3e085731c91abb7ad4fcdb1329
zax <2114531580@qq.com>
2023-07-20 11:55:21 +0800

    自增自减运算

--- src/eg01/IncrementDemo.java
+++ src/eg01/IncrementDemo.java
@@ -9,5 +9,10 @@ public class IncrementDemo {
         System.out.println(i++);//先取值,再自增
         System.out.println(i);
         System.out.println(++i);//先自增,再取值
+
+        int a=1;
+        System.out.println(a--);//先取值,再自减
+        System.out.println(a);
+        System.out.println(--a);//先自减,再取值
     }
 }
