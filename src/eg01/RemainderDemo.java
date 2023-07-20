a065719c7e45623f4696728260a98c27fdcf0db1
zax <2114531580@qq.com>
2023-07-20 11:43:16 +0800

    取余运算

---
+++ src/eg01/RemainderDemo.java
@@ -0,0 +1,14 @@
+package eg01;
+
+/**
+ * 取余运算(取模运算)
+ */
+public class RemainderDemo {
+    public static void main(String[] args) {
+        System.out.println(0%3);
+        System.out.println(1%3);
+        System.out.println(2%3);
+        System.out.println(3%3);
+        System.out.println(4%3);
+    }
+}
